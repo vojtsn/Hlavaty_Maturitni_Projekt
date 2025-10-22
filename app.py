1beeb582250f15e0ab56d0bed1b518436ab932f0
vojta hlavaty <vojtahlavatyy@gmail.com>
2025-10-23 00:02:02 +0200

    Připojení k databázi
    Registrace a login uživatele skrz databázi (vyuka11, users)

--- app.py
+++ app.py
@@ -1,35 +1,75 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import re
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'tajny_klic'  # změň na vlastní
+app.config['SECRET_KEY'] = 'tajny_klic'
 
-# --- Uživatelská data napevno ---
-USERS = {
-    "admin": "admin123",
-    "ctenar": "heslo"
-}
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://student11:spsnet@dbs.spskladno.cz:3306/vyuka11"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Homepage
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+# --- Domovská stránka ---
 @app.route('/')
 def index():
     username = session.get('username')
     return render_template('index.html', username=username)
 
-# Přihlášení
+# --- Přihlášení ---
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username in USERS and USERS[username] == password:
-            session['username'] = username
+
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['username'] = user.username
             return redirect(url_for('index'))
         else:
             error = "Špatné jméno nebo heslo."
     return render_template('login.html', error=error)
 
-# Odhlášení
+# --- Registrace ---
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Kontrola unikátnosti
+        if User.query.filter_by(username=username).first():
+            error = "Uživatelské jméno je již obsazeno."
+        elif User.query.filter_by(email=email).first():
+            error = "Email je již použit."
+        # Kontrola hesla: alespoň 1 velké písmeno a 1 číslo
+        elif not re.search(r'[A-Z]', password):
+            error = "Heslo musí obsahovat alespoň 1 velké písmeno."
+        elif not re.search(r'\d', password):
+            error = "Heslo musí obsahovat alespoň 1 číslo."
+        else:
+            hashed_password = generate_password_hash(password)
+            new_user = User(username=username, email=email, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect(url_for('index'))
+
+    return render_template('register.html', error=error)
+
+
+# --- Odhlášení ---
 @app.route('/logout')
 def logout():
     session.pop('username', None)
@@ -37,3 +77,4 @@ def logout():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
